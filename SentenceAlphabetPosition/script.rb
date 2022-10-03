1ae262a6680f25f46d84d80daff0f628ff8d0a63
Andrii Klochko <andrii.k@aquimarketing.com>
2022-10-03 11:12:26 +0300

    Minor updates

--- SentenceAlphabetPosition/script.rb
+++ SentenceAlphabetPosition/script.rb
@@ -1,21 +1,17 @@
 input_string = 'The sunset sets at twelve o\' clock.'
 puts input_string
 
-def get_letter_code(letter)
-  letter.ord
-end
-
-def get_alphabet_position(string)
+def alphabet_position(string)
   first_letter_code = 'a'.ord
   last_letter_code = 'z'.ord
 
   "#{string
        .downcase
        .split('')
-       .map { |letter| get_letter_code(letter) }
+       .map { |letter| letter.ord }
        .select { |letter_code| letter_code >= first_letter_code && letter_code <= last_letter_code }
        .map { |letter_code| letter_code - first_letter_code + 1 }}
   ".gsub(",", "")
 end
 
-puts get_alphabet_position(input_string)
+puts alphabet_position(input_string)
