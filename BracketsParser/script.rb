1bf0df76dbd4c9e380f51214514526f95c4b5356
Andrii Klochko <andrii.k@aquimarketing.com>
2022-10-06 22:55:40 +0300

    Minor improvements

--- BracketsParser/script.rb
+++ BracketsParser/script.rb
@@ -1,19 +1,13 @@
 require_relative('input_utils')
 
-def is_string_brackets_correct(string)
+def is_string_brackets_correct(string, key_value_schema)
   if string.length % 2 != 0
     return false
   end
 
-  brackets_key_value_object = {
-    '(' => ')',
-    '[' => ']',
-    '{' => '}',
-  }
-
   closed_brackets_array = []
   string.chars do |char|
-    if (closed_bracket_by_open_bracket = brackets_key_value_object[char])
+    if (closed_bracket_by_open_bracket = key_value_schema[char])
       closed_brackets_array << closed_bracket_by_open_bracket
     elsif closed_brackets_array.pop != char
       return false
@@ -23,14 +17,19 @@ def is_string_brackets_correct(string)
   closed_brackets_array.length === 0
 end
 
-brackets = '()[]{}'
+brackets_key_value_object = {
+  '(' => ')',
+  '[' => ']',
+  '{' => '}',
+}
+all_brackets_string = brackets_key_value_object.keys + brackets_key_value_object.values
 input_string = ''
 
 while 1
   puts 'Enter bracket: '
   letter = input_letter
 
-  unless brackets.include? letter
+  unless all_brackets_string.include? letter
     next
   end
 
@@ -38,6 +37,6 @@ while 1
   puts 'Your string: ' + input_string
 
   print 'Is string correct: '
-  puts is_string_brackets_correct(input_string)
+  puts is_string_brackets_correct(input_string, brackets_key_value_object)
   puts ''
 end
